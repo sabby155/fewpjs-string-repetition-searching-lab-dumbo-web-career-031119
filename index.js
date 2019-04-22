25cc2fccfee0804732516c53c7874c013715501f
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-22 15:57:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+
+function heyJude(verse) {
+ return verse.repeat(16);
+}
+
+
+function prohibitedLanguage(candy) {
+  let ban = /candycorn|brusselssprouts/;
+  return candy.search(ban);
+}
