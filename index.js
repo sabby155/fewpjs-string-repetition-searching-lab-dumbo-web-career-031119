7c5590a591a357958bf3a59075e2a4d73d1a29cd
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-22 15:53:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Code your solution here
 
 function heyJude() {
-  let na = 'na '.repeat(16);
-  let result = `${na}, hey Jude.`
-  return result.charAt(0).toUpperCase() + result.slice(1);
+ verse.repeat(16);
 }
